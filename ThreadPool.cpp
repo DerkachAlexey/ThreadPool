04c144fe615785a4119433d9130606eeaa74a375
DerkachAlexey <alexey.derkach@gmail.com>
2022-01-22 23:28:50 +0200

    Changed implementation of test func. Added ability to return std::future

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,73 +1,75 @@
 #include "ThreadPool.hpp"
+#include <iostream>
 
 namespace tpl {
 
-	ThreadPool::ThreadPool() :
-		// we suppose that our OS will tell us how many threads
-		// are available, i.e. numberOfThreads always > 0
-		m_numberOfThreads(std::thread::hardware_concurrency()),
-		m_isRunning(true)
+ThreadPool::ThreadPool() :
+	// we suppose that our OS will tell us how many threads
+	// are available, i.e. numberOfThreads always > 0
+	m_numberOfThreads(std::thread::hardware_concurrency()),
+	m_isRunning(true)
+{
+	initializeThreads();
+}
+
+ThreadPool::~ThreadPool()
+{
+	m_isRunning.store(false);
+
+	m_threadsWaiter.notify_all();
+
+	for (auto& thread : m_threads)
 	{
-		initializeThreads();
-	}
-
-	ThreadPool::~ThreadPool()
-	{
-		m_isRunning.store(false);
-
-		m_threadsWaiter.notify_all();
-
-		for (auto& thread : m_threads)
-		{
-			thread.join();
-		}
-	}
-
-	void ThreadPool::initializeThreads()
-	{
-		for (size_t i = 0; i < m_numberOfThreads; ++i)
-		{
-			m_threads.push_back(std::thread(&ThreadPool::dummyWorker, this));
-		}
-	}
-
-	void ThreadPool::dummyWorker()
-	{
-		// kill thread if running is false
-		while (m_isRunning.load())
-		{
-			std::function<void()> runable;
-
-			std::unique_lock<std::mutex> lock(m_dequeMutex);
-
-			m_threadsWaiter.wait(lock, [this]() {return !m_tasks.empty() || !m_isRunning.load(); });
-
-			// tasks queue may be empty after awakening since we use !m_isRunning.load()
-			// in the predicate to escape dead lock when we join the thread and wait
-			// for the conditional variable however there are no tasks
-			if (!m_tasks.empty())
-			{
-				runable = m_tasks.front();
-				m_tasks.pop_front();
-			}
-
-			lock.unlock(); // unlock to allow other threads perform tasks
-
-			if (runable)
-			{
-				runable();
-			}
-		}
-	}
-
-	ThreadPool & ThreadPool::getInstance()
-	{
-		static ThreadPool threadPool;
-		return threadPool;
-	}
-
-	uint64_t ThreadPool::getNumberOfThreads() const
-	{
-		return m_numberOfThreads;
+		thread.join();
 	}
 }
+
+void ThreadPool::initializeThreads()
+{
+	for (size_t i = 0; i < m_numberOfThreads; ++i)
+	{
+		m_threads.push_back(std::thread(&ThreadPool::dummyWorker, this));
+	}
+}
+
+void ThreadPool::dummyWorker()
+{
+	// kill thread if running is false
+	while (m_isRunning.load())
+	{
+		std::function<void()> runable;
+
+		std::unique_lock<std::mutex> lock(m_dequeMutex);
+
+		m_threadsWaiter.wait(lock, [this]() {return !m_tasks.empty() || !m_isRunning.load(); });
+
+		// tasks queue may be empty after awakening since we use !m_isRunning.load()
+		// in the predicate to escape dead lock when we join the thread and wait
+		// for the conditional variable however there are no tasks
+		if (!m_tasks.empty())
+		{
+			runable = m_tasks.front();
+			m_tasks.pop_front();
+		}
+
+		lock.unlock(); // unlock to allow other threads perform tasks
+
+		if (runable)
+		{
+			runable();
+		}
+	}
+}
+
+ThreadPool& ThreadPool::getInstance()
+{
+	static ThreadPool threadPool;
+	return threadPool;
+}
+
+uint64_t ThreadPool::getNumberOfThreads() const
+{
+	return m_numberOfThreads;
+}
+
+} // namespace tpl
--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -5,6 +5,7 @@
 #include <condition_variable>
 #include <atomic>
 #include <memory>
+#include <future>
 
 #ifndef THREAD_POOL
 #define THREAD_POOL
@@ -12,49 +13,94 @@
 //tpl - thread pool library
 namespace tpl {
 
-	class ThreadPool
+class ThreadPool
+{
+	using Runable = std::function<void()>;
+
+public:
+	static ThreadPool& getInstance();
+
+	template<typename TFuncToRun, typename ...TFuncParams,
+		typename = std::enable_if_t<std::is_void_v<std::invoke_result_t<std::decay_t<TFuncToRun>, std::decay_t<TFuncParams>...>>>>
+	std::future<bool> run(const TFuncToRun& funcToRun, const TFuncParams& ...funcParams)
 	{
-		using Runable = std::function<void()>;
+		// we use bool to determine if the task is successfully completed
+		auto sharedPtrPromise = std::make_shared<std::promise<bool>>();
+		std::future<bool> funcFuture = sharedPtrPromise->get_future();
 
-	public:
-		static ThreadPool& getInstance();
+		std::unique_lock<std::mutex> lock(m_dequeMutex);
 
-		template<typename TFuncToRun, typename ...TFuncParams>
-		void run(const TFuncToRun& funcToRun, TFuncParams&& ...funcParams)
+		m_tasks.push_back([funcToRun, funcParams..., sharedPtrPromise]()
 		{
-			std::unique_lock<std::mutex> lock(m_dequeMutex);
-
-			m_tasks.push_back([&funcToRun, &funcParams...]()
+			try
 			{
-				funcToRun(std::forward<TFuncParams>(funcParams)...); 
-			});
+				funcToRun(funcParams...);
+				sharedPtrPromise->set_value(true);
+			}
+			catch (...)
+			{
+				sharedPtrPromise->set_exception(std::current_exception());
+			}
+		});
 
-			lock.unlock();
-			m_threadsWaiter.notify_one();
-		}
-		uint64_t getNumberOfThreads() const;
+		lock.unlock();
+		m_threadsWaiter.notify_one();
 
-		ThreadPool(const ThreadPool&) = delete;
-		ThreadPool(ThreadPool&& movablePool) = delete;
-		ThreadPool& operator = (const ThreadPool&) = delete;
-		ThreadPool& operator = (ThreadPool&& movablePool) = delete;
+		return funcFuture;
+	}
 
-		~ThreadPool();
 
-	private:
-		ThreadPool();
+	template<typename TFuncToRun, typename ...TFuncParams,
+		typename TReturn = std::invoke_result_t<std::decay_t<TFuncToRun>, std::decay_t<TFuncParams>...>,
+		typename = std::enable_if_t<!std::is_void_v<TReturn>>>
+	std::future<TReturn> run(const TFuncToRun& funcToRun, const TFuncParams& ...funcParams)
+	{
+		auto sharedPtrPromise = std::make_shared<std::promise<TReturn>>();
+		std::future<TReturn> funcFuture = sharedPtrPromise->get_future();
 
-		void initializeThreads();
-		void dummyWorker();
+		std::unique_lock<std::mutex> lock(m_dequeMutex);
 
-	private:
-		size_t m_numberOfThreads;
-		std::vector<std::thread> m_threads;
-		std::deque<Runable> m_tasks;
-		std::condition_variable m_threadsWaiter;
-		std::mutex m_dequeMutex;
-		std::atomic<bool> m_isRunning;
-	};
+		m_tasks.push_back([funcToRun, funcParams..., sharedPtrPromise]()
+		{
+			try
+			{
+				sharedPtrPromise->set_value(funcToRun(funcParams...));
+			}
+			catch (...)
+			{
+				sharedPtrPromise->set_exception(std::current_exception());
+			}
+		});
+
+		lock.unlock();
+		m_threadsWaiter.notify_one();
+
+		return funcFuture;
+	}
+
+	uint64_t getNumberOfThreads() const;
+
+	ThreadPool(const ThreadPool&) = delete;
+	ThreadPool(ThreadPool&& movablePool) = delete;
+	ThreadPool& operator = (const ThreadPool&) = delete;
+	ThreadPool& operator = (ThreadPool&& movablePool) = delete;
+
+	~ThreadPool();
+
+private:
+	ThreadPool();
+
+	void initializeThreads();
+	void dummyWorker();
+
+private:
+	size_t m_numberOfThreads;
+	std::vector<std::thread> m_threads;
+	std::deque<Runable> m_tasks;
+	std::condition_variable m_threadsWaiter;
+	std::mutex m_dequeMutex;
+	std::atomic<bool> m_isRunning;
+};
 }
 
 #endif // !THREAD_POOL
